2dad525b985c1ea596e0ce8300b09a2f29d34d7c
wportilh <willianportilho01@gmail.com>
2023-02-08 14:31:37 -0300

    added conversion argv to uppercase

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main (void)
+int main (int argc, char *argv[])
 {
-	std::cout << "Hello World!\n";
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (unsigned char)std::toupper(argv[i][j]);
+			std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+	return (0);
 }
