4ad99275f2b71bff8b2535c10fd7d9df1f8c58ea
wportilh <willianportilho01@gmail.com>
2023-02-16 14:15:09 -0300

    renamed kratos to ashley.

--- module_01/ex00/main.cpp
+++ module_01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 18:22:45 by wportilh          #+#    #+#             */
-/*   Updated: 2023/02/16 14:10:58 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/02/16 14:14:06 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int main (void)
 	Zombie	michael("Leon Kennedy");
 	michael.announce();
 
-	Zombie	*kratos;
-	kratos = newZombie("Ashley Graham");
-	kratos->announce();
-	delete	kratos;
+	Zombie	*ashley;
+	ashley = newZombie("Ashley Graham");
+	ashley->announce();
+	delete	ashley;
 
 	randomChump("Ada Wong");
 
