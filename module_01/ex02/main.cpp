b1dded89c941f48eba33b60ffcaa911745c6adb7
wportilh <willianportilho01@gmail.com>
2023-02-16 18:44:09 -0300

    updating main.yml (.github/workflows/); completed ex02 (module_01/)

--- module_01/ex02/main.cpp
+++ module_01/ex02/main.cpp
@@ -6,13 +6,34 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 18:05:17 by wportilh          #+#    #+#             */
-/*   Updated: 2023/02/16 18:26:28 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/02/16 18:42:00 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 int	main(void)
 {
+	std::string	string = "HI THIS IS BRAIN";
+
+	std::string	*stringPTR = &string;
+
+	std::string	&stringREF = string;
+
+	std::cout << "memory address - string   : "
+	<< &string << std::endl;
+	std::cout << "memory address - stringPTR: "
+	<< stringPTR << std::endl;
+	std::cout << "memory address - stringREF: "
+	<< &stringREF << std::endl;
+
+	std::cout << "value - string   : "
+	<< string << std::endl;
+	std::cout << "value - stringPTR: "
+	<< *stringPTR << std::endl;
+	std::cout << "value - stringREF: "
+	<< stringREF << std::endl;
+
 	return (0);
 }
