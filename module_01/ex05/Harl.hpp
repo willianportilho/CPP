a6e2224f3fb3fbcc279a36c0296fee87bb830f82
wportilh <willianportilho01@gmail.com>
2023-02-19 15:30:31 -0300

    added typedef pointers; added error protection; added main tests.

--- module_01/ex05/Harl.hpp
+++ module_01/ex05/Harl.hpp
@@ -6,18 +6,28 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 10:26:09 by wportilh          #+#    #+#             */
-/*   Updated: 2023/02/18 10:29:01 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/02/19 15:25:10 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HARL_H
 # define HARL_H
 
+#include <string>
+#include <iostream>
+
 class Harl
 {
 	public:
 		Harl(void);
 		~Harl(void);
+		void	complain(std::string level);
+	private:
+		void _debug(void);
+		void _info(void);
+		void _warning(void);
+		void _error(void);
+		typedef	void (Harl::*_f) (void);
 };
 
 #endif
