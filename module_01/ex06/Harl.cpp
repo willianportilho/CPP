d6fba3c0a931c8225677c2d90c70865d0da901bb
wportilh <willianportilho01@gmail.com>
2023-02-19 16:15:54 -0300

    added ex06/ folder (module_01/); removed argc argv (module_01/ex05/)

---
+++ module_01/ex06/Harl.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 10:27:37 by wportilh          #+#    #+#             */
+/*   Updated: 2023/02/19 15:35:34 by wportilh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	return ;
+}
+
+Harl::~Harl(void)
+{
+	return ;
+}
+
+void	Harl::_debug(void)
+{
+	std::cout
+	<< "I love having extra bacon for my"
+	<< "7XL-double-cheese-triple-pickle-special-ketchup burger. "
+	<< std::endl
+	<< "I really do!"
+	<< std::endl;
+	return ;
+}
+
+void	Harl::_info(void)
+{
+	std::cout
+	<< "I cannot believe adding extra bacon costs more money." 
+	<< std::endl
+	<< "You didn't put enough bacon in my burger! "
+	<< std::endl
+	<< "If you did, I wouldn't be asking for more!"
+	<< std::endl;
+	return ;
+}
+
+void	Harl::_warning(void)
+{
+	std::cout
+	<< "I think I deserve to have some extra bacon for free. "
+	<< std::endl
+	<< "I've been coming for years whereas you started working "
+	<< "here since last month."
+	<< std::endl;
+	return ;
+}
+
+void	Harl::_error(void)
+{
+	std::cout
+	<< "This is unacceptable! I want to speak to the manager now."
+	<< std::endl;
+	return ;
+}
+
+void	Harl::complain(std::string level)
+{
+	Harl::_f 	p_level[4] = \
+	{&Harl::_debug, &Harl::_info, &Harl::_warning, &Harl::_error};
+	std::string	s_level[4] = \
+	{"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == s_level[i])
+		{
+			(this->*p_level[i])();
+			return ;
+		}
+	}
+	std::cerr << "harl: error: invalid level" << std::endl;
+	return ;
+}
--- module_01/ex06/main.cpp
+++ module_01/ex06/main.cpp
@@ -6,43 +6,19 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 18:05:17 by wportilh          #+#    #+#             */
-/*   Updated: 2023/02/19 16:10:37 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/02/19 16:14:13 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
 	Harl	level;
-
-	std::cout << "[ DEBUG ]: " << "valid" << std::endl;
-	level.complain("DEBUG");
-	std::cout << std::endl;
-
-	std::cout << "[ INFO ]: " << "valid" << std::endl;
-	level.complain("INFO");
-	std::cout << std::endl;
-
-	std::cout << "[ WARNING ]: " << "valid" << std::endl;
-	level.complain("WARNING");
-	std::cout << std::endl;
-
-	std::cout << "[ ERROR ]: " << "valid" << std::endl;
-	level.complain("ERROR");
-	std::cout << std::endl;
-
-	std::cout << "[ TEST ]: " << "invalid" << std::endl;
-	level.complain("TEST");
-	std::cout << std::endl;
-
-	std::cout << "[ 0123456789 ]: " << "invalid" << std::endl;
-	level.complain("0123456789");
-	std::cout << std::endl;
-
-	std::cout << "[ TWO WORDS ]: " << "invalid" << std::endl;
-	level.complain("TWO WORDS");
-	std::cout << std::endl;
-
+	if (argc != 2)
+	{	std::cout << "harlFilter: error: invalid number of arguments"
+		<< std::endl;
+		return (-1);
+	}
 	return (0);
 }
