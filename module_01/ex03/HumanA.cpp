0890d73587394dab74791aaffed06dd4d116d80e
wportilh <willianportilho01@gmail.com>
2023-02-22 11:32:49 -0300

    fixed initialization HumanA and HumanB (module_01/ex03/).

--- module_01/ex03/HumanA.cpp
+++ module_01/ex03/HumanA.cpp
@@ -6,13 +6,13 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 21:45:55 by wportilh          #+#    #+#             */
-/*   Updated: 2023/02/17 10:29:58 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/02/22 11:28:13 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string name, Weapon &weapon) : _name(name), _weapon(weapon)
+HumanA::HumanA(std::string name, Weapon &weapon) : _weapon(weapon), _name(name)
 {
 	return ;
 }
--- module_02/ex00/main.cpp
+++ module_02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 18:05:17 by wportilh          #+#    #+#             */
-/*   Updated: 2023/02/22 11:28:53 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/02/22 11:30:44 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(void)
 {
 	Fixed	instance;
+	Fixed	instance2(instance);
 
 	return (0);
 }
