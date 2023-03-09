f66b1b95949592a2e3909794130fbbc8bf46b0e4
wportilh <willianportilho01@gmail.com>
2023-03-08 22:05:31 -0300

    added getName() and getGrade(); added exceptions (High and Low).

--- module_05/ex00/Bureaucrat.cpp
+++ module_05/ex00/Bureaucrat.cpp
@@ -6,15 +6,29 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 13:49:12 by wportilh          #+#    #+#             */
-/*   Updated: 2023/03/07 21:17:41 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/03/08 21:58:59 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(void)
+Bureaucrat::Bureaucrat(void) : _name(""), _grade(42)
 {
+	if (getGrade() < 1)
+		std::cout << "throw smallest number";
+	if (getGrade() > 150)
+		std::cout << "throw greatest number";
 	std::cout << "default Bureaucrat constructor called" << std::endl;
+	return ;
+}
+
+Bureaucrat::Bureaucrat(unsigned int grade, std::string const name) : _name(name), _grade(grade)
+{
+	if (getGrade() < 1)
+		std::cout << "throw smallest number";
+	if (getGrade() > 150)
+		std::cout << "throw greatest number";
+	std::cout << "Bureaucrat constructor called with parameters (grade and name)" << std::endl;
 	return ;
 }
 
@@ -39,4 +53,24 @@ Bureaucrat	&Bureaucrat::operator=(Bureaucrat const &rhs)
 	}
 	std::cout << "assignment copy Bureaucrat called" << std::endl;
 	return (*this);
+}
+
+const char	*Bureaucrat::GradeTooHighException::what(void) const throw()
+{
+	return ("error: GradeTooHighException: The grade is too high!");
+}
+
+const char	*Bureaucrat::GradeTooLowException::what(void) const throw()
+{
+	return ("error: GradeTooLowException: The grade is too low!");
+}
+
+std::string const	Bureaucrat::getName(void)
+{
+	return (this->_name);
+}
+
+unsigned int	Bureaucrat::getGrade(void)
+{
+	return (this->_grade);
 }
--- module_05/ex00/Bureaucrat.hpp
+++ module_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 13:45:11 by wportilh          #+#    #+#             */
-/*   Updated: 2023/03/07 21:16:29 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/03/08 21:59:17 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,26 @@
 class	Bureaucrat
 {
 	public:
-		Bureaucrat(void);
+		Bureaucrat(unsigned int grade, std::string const name);
 		Bureaucrat(Bureaucrat const &src);
 		~Bureaucrat(void);
-		Bureaucrat		&operator=(Bureaucrat const &rhs);
+		Bureaucrat			&operator=(Bureaucrat const &rhs);
+		std::string const	getName(void);
+		unsigned int		getGrade(void);
+		class	GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char	*what(void) const throw();
+		};
+		class	GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char	*what(void) const throw();
+		};
+	private:
+		Bureaucrat(void);
+		std::string const	_name;
+		unsigned int		_grade;
 };
 
 #endif
