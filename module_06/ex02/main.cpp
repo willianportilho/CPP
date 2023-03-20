c6be1ec9fa49944bc46209228aa9b69d18ad94a5
wportilh <willianportilho01@gmail.com>
2023-03-20 09:24:20 -0300

    added implemented identify() functions (reference and pointer)

--- module_06/ex02/main.cpp
+++ module_06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 18:02:42 by wportilh          #+#    #+#             */
-/*   Updated: 2023/03/19 22:39:01 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/03/20 09:07:37 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,13 @@ Base* generate(void)
 	switch (randow)
 	{
 		case 0:
+			std::cout << "generate returned \"A\" class" << std::endl;
 			return (new A);	
 		case 1:
+			std::cout << "generate returned \"B\" class" << std::endl;
 			return (new B);
 		case 2:
+			std::cout << "generate returned \"C\" class" << std::endl;
 			return (new C);
 	}
 	return (NULL);
@@ -38,17 +41,67 @@ Base* generate(void)
 
 void identify(Base* p)
 {
-	(void)p;
+	A *a = dynamic_cast<A*>(p);
+	B *b = dynamic_cast<B*>(p);
+	C *c = dynamic_cast<C*>(p);
+
+	if (a)
+		std::cout << "A (by pointer)" << std::endl;
+	else if (b)
+		std::cout << "B (by pointer)" << std::endl;
+	else if (c)
+		std::cout << "C (by pointer)" << std::endl;
+	else
+		std::cerr << "identify: error: something went wrong (identify pointer)" << std::endl;
 	return ;
 }
 
 void identify(Base& p)
 {
-	(void)p;
+	try
+	{
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A (by reference)" << std::endl;
+		return ;
+	}
+	catch(...)
+	{
+	}
+
+	try
+	{
+		B &b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "B (by reference)" << std::endl;
+		return ;
+	}
+	catch(...)
+	{
+	}
+
+	try
+	{
+		C &c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "C (by reference)" << std::endl;
+		return ;
+	}
+	catch(...)
+	{
+	}
+
+	std::cerr << "identify: error: something went wrong (identify reference)" << std::endl;
+
 	return ;	
 }
 
 int	main(void)
 {
+	Base *test = generate();
+
+	std::cout << std::endl << "Result:" << std::endl;
+	identify(test);
+	identify(*test);
 	return (0);
 }
