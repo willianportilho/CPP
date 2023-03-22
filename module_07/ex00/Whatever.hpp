e1c8292c8fdf3544ccce72033c3453cb43214b69
wportilh <willianportilho01@gmail.com>
2023-03-21 21:02:21 -0300

    added swap(), min() and max() functions (module_07/ex00/Whatever.hpp); added subject tests (module_07/ex00/main.cpp)

--- module_07/ex00/Whatever.hpp
+++ module_07/ex00/Whatever.hpp
@@ -6,15 +6,39 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 13:45:11 by wportilh          #+#    #+#             */
-/*   Updated: 2023/03/21 20:36:28 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/03/21 20:59:06 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_H
 # define WHATEVER_H
 
-#include <iostream>
-#include <string>
+template<typename T, typename U>
+void	swap(T &a, U &b)
+{
+	T	tmp;
 
+	tmp = a;
+	a = b;
+	b = tmp;
+
+	return ;
+}
+
+template<typename T, typename U>
+T const	&min(T const &a, const U &b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+template<typename T, typename U>
+T const	&max(T const &a, U const &b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
 
 #endif
--- module_07/ex00/main.cpp
+++ module_07/ex00/main.cpp
@@ -6,14 +6,26 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 18:02:42 by wportilh          #+#    #+#             */
-/*   Updated: 2023/03/21 20:36:58 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/03/21 20:59:19 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Whatever.hpp"
+#include <iostream>
 
 int	main(void)
 {
-	
-	return (0);
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
