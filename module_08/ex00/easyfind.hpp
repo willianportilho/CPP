2d13d1069089a8c316bda60bd07577fa977d8009
wportilh <willianportilho01@gmail.com>
2023-03-27 15:05:01 -0300

    added module_08; added ex00/ (module_08/ex00/).

---
+++ module_08/ex00/easyfind.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/03 13:45:11 by wportilh          #+#    #+#             */
+/*   Updated: 2023/03/27 15:04:02 by wportilh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_H
+# define EASYFIND_H
+
+template<typename T>
+void	easyfind(T &a)
+{
+	(void)a;
+	return ;
+}
+
+#endif
