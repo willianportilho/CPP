2f7da6fb7a05d0890a834dbe0474214f3b31a606
wportilh <willianportilho01@gmail.com>
2023-03-30 19:22:38 -0300

    implemented canonical form in the exceptions (module_08/ex00/).

--- module_08/ex00/NumberNotFoundException.cpp
+++ module_08/ex00/NumberNotFoundException.cpp
@@ -6,11 +6,36 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 13:45:11 by wportilh          #+#    #+#             */
-/*   Updated: 2023/03/28 14:23:20 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/03/30 19:20:50 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "NumberNotFoundException.hpp"
+
+NumberNotFoundException::NumberNotFoundException(void)
+{
+	return ;
+}
+
+NumberNotFoundException::NumberNotFoundException(NumberNotFoundException const &src)
+{
+	*this = src;
+	return ;
+}
+
+NumberNotFoundException	&NumberNotFoundException::operator=(NumberNotFoundException const &rhs)
+{
+	if (this != &rhs)
+	{
+		;
+	}
+	return (*this);
+}
+
+NumberNotFoundException::~NumberNotFoundException(void) throw()
+{
+	return ;
+}
 
 const char *NumberNotFoundException::what(void) const throw()
 {
--- module_08/ex00/NumberNotFoundException.hpp
+++ module_08/ex00/NumberNotFoundException.hpp
@@ -6,19 +6,24 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 13:45:11 by wportilh          #+#    #+#             */
-/*   Updated: 2023/03/28 14:23:46 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/03/30 19:20:07 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXCEPTIONS_H
-# define EXCEPTIONS_H
+#ifndef NumberNotFoundException_H
+# define NumberNotFoundException_H
 
 #include <exception>
 
 class	NumberNotFoundException : public std::exception
 {
 	public:
+		NumberNotFoundException(void);
+		NumberNotFoundException	&operator=(NumberNotFoundException const &rhs);
+		NumberNotFoundException(NumberNotFoundException const &src);
+		virtual ~NumberNotFoundException(void) throw ();
 		virtual const char	*what(void) const throw();
+	private:
 };
 
 #endif
