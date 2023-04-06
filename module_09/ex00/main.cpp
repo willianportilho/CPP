97c57e790fd96a4cdcce9ab921b61aec3012e050
wportilh <willianportilho01@gmail.com>
2023-04-06 14:58:08 -0300

    added main.cpp

--- module_09/ex00/main.cpp
+++ module_09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 14:59:23 by wportilh          #+#    #+#             */
-/*   Updated: 2023/04/06 14:10:03 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/04/06 14:51:16 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,13 @@
 #include <iostream>
 #include <string>
 
-static void	printTitle(std::string const title)
-{
-	std::cout << std::endl;
-	std::cout << "=================" << std::endl;
-	std::cout << "=================" << std::endl;
-	std::cout << title << std::endl;
-	std::cout << "=================" << std::endl;
-	std::cout << "=================" << std::endl;
-	return ;
-}
-
-int main()
-{
-	printTitle("Teste");
-
+int main(int argc, char **argv)
+{	
+	if (argc != 2)
+	{
+		std::cerr << "btc: error: invalid number of arguments" << std::endl;
+		return (1);
+	}
+	(void)argv;
 	return (0);
 }
