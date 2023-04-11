048c809bdfc71b77472997cfbc32ba213cfc269e
wportilh <willianportilho01@gmail.com>
2023-04-10 22:14:14 -0300

    removed _fillStack() function; added _calcExpression(), _isOperator(), _getOperation(), _calc(), _checkFinalResult() and _printResult() functions; added _operator, _operandA, _operandB and _result lon lon int attributes.

--- module_09/ex01/RPN.cpp
+++ module_09/ex01/RPN.cpp
@@ -6,13 +6,16 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 13:45:11 by wportilh          #+#    #+#             */
-/*   Updated: 2023/04/10 17:54:06 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/04/10 22:01:44 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
-RPN::RPN(void) : _plus('+'), _minus('-'), _multiple('*'), _divide('/'), _space(' '), _zero('0')
+RPN::RPN(void) :	_operator(' '),
+					_operandA(0), _operandB(0), _result(0),
+					 _plus('+'), _minus('-'), _multiple('*'),
+					_divide('/'), _space(' '), _zero('0')
 {
 	return ;
 }
@@ -29,9 +32,11 @@ RPN	&RPN::operator=(RPN const &rhs)
 {
 	if (this != &rhs)
 	{
-		// this->_stack.;
 		// this->_stack = rhs._stack;
-		;
+		this->_operator = rhs._operator;
+		this->_operandA	= rhs._operandA;
+		this->_operandB	= rhs._operandB;
+		this->_result	= rhs._result;
 	}
 
 	return (*this);
@@ -42,7 +47,7 @@ RPN::~RPN(void)
 	return ;
 }
 
-int		RPN::_isValidCharacter(char c)
+bool		RPN::_isValidCharacter(char c)
 {
 	if ((!isdigit(c)) && (c != this->_plus) && (c != this->_minus)
 	&& (c != this->_multiple) && (c != this->_divide) && (c != this->_space))
@@ -131,10 +136,88 @@ void	RPN::_handleExpression(std::string &expression)
 	return ;
 }
 
-void	RPN::_fillStack(std::string &expression)
+bool	RPN::_isOperator(char c)
+{
+	if ((c != this->_plus) && (c != this->_minus)
+	&& (c != this->_multiple) && (c != this->_divide))
+		return (false);
+
+	return (true);
+}
+
+void	RPN::_getOperation(char operatorType)
+{
+	this->_operandB = this->_stack.top();
+	this->_stack.pop();
+	this->_operandA = this->_stack.top();
+	this->_stack.pop();
+
+	this->_operator = operatorType;
+
+	return ;
+}
+
+void	RPN::_calc(void)
+{
+	switch (this->_operator)
+	{
+	case '+':
+		this->_result = this->_operandA + this->_operandB;
+		break;
+	case '-':
+		this->_result = this->_operandA - this->_operandB;
+		break;
+	case '*':
+		this->_result = this->_operandA * this->_operandB;
+		break;
+	case '/':
+		if (this->_operandB == 0)
+			throw Exceptions("division by 0 detected");
+		this->_result = this->_operandA / this->_operandB;
+		break;
+	default:
+		throw Exceptions("something went wrong");
+		break;
+	}
+
+	this->_stack.push(this->_result);
+
+	return ;
+}
+
+void	RPN::_checkFinalResult(void)
+{
+	if (this->_stack.size() != 1)
+		throw Exceptions("invalid expression");
+
+	return ;
+}
+
+void	RPN::_calcExpression(std::string &expression)
 {
 	for (int i = 0; expression[i]; i++)
-		this->_stack.push(expression[i]);	
+	{
+		if (isdigit(expression[i]))
+			this->_stack.push(expression[i] - '0');
+		else if (_isOperator(expression[i]) == true)
+		{
+			if (this->_stack.size() > 1)
+			{
+				_getOperation(expression[i]);
+				_calc();
+			}
+			else
+				throw Exceptions("invalid expression");
+		}
+	}
+	_checkFinalResult();
+
+	return ;
+}
+
+void	RPN::_printResult(void)
+{
+	std::cout << this->_stack.top() << std::endl;
 
 	return ;
 }
@@ -142,7 +225,8 @@ void	RPN::_fillStack(std::string &expression)
 void	RPN::reversePolishNotation(std::string expression)
 {
 	_handleExpression(expression);
-	_fillStack(expression);
+	_calcExpression(expression);
+	_printResult();
 
 	return ;
 }
--- module_09/ex01/RPN.hpp
+++ module_09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 13:58:14 by wportilh          #+#    #+#             */
-/*   Updated: 2023/04/10 17:45:09 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/04/10 21:44:16 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <string>
 #include <iostream>
 #include <stack>
+#include <stdlib.h>
 #include "Exceptions.hpp"
 
 class	RPN
@@ -24,24 +25,33 @@ class	RPN
 		RPN(void);
 		RPN(RPN const &src);
 		~RPN(void);
-		RPN					&operator=(RPN const &rhs);
-		void				reversePolishNotation(std::string expression);
+		RPN							&operator=(RPN const &rhs);
+		void						reversePolishNotation(std::string expression);
 	private:
-		std::stack<char>	_stack;
-		int					_isValidCharacter(char c);
-		void				_checkCharacters(std::string &expression);
-		void				_removeLeadingZeros(std::string &expression);
-		void				_checkValues(std::string &expression);
-		void				_removeSpaces(std::string &expression);
-		void				_checkMinimumExpression(std::string &expression);
-		void				_handleExpression(std::string &expression);
-		void				_fillStack(std::string &expression);
-		const char			_plus;
-		const char			_minus;
-		const char			_multiple;
-		const char			_divide;
-		const char			_space;
-		const char			_zero;
+		char						_operator;
+		long long int				_operandA;
+		long long int				_operandB;
+		long long int				_result;
+		std::stack<long long int>	_stack;
+		bool						_isValidCharacter(char c);
+		void						_checkCharacters(std::string &expression);
+		void						_removeLeadingZeros(std::string &expression);
+		void						_checkValues(std::string &expression);
+		void						_removeSpaces(std::string &expression);
+		void						_checkMinimumExpression(std::string &expression);
+		void						_handleExpression(std::string &expression);
+		bool						_isOperator(char c);
+		void						_calc(void);
+		void						_getOperation(char operatorType);
+		void						_checkFinalResult(void);
+		void						_calcExpression(std::string &expression);
+		void						_printResult(void);
+		const char					_plus;
+		const char					_minus;
+		const char					_multiple;
+		const char					_divide;
+		const char					_space;
+		const char					_zero;
 };
 
 #endif
